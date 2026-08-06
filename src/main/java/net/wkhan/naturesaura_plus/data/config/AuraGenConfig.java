49eefbe2a08c1ad487ac532f0bcc621b69da7aa4
Waqar Ahmad Khan <wak4746@gmail.com>
2026-08-06 20:02:17 +0530

    better looking configs

--- src/main/java/net/wkhan/naturesaura_plus/data/config/AuraGenConfig.java
+++ src/main/java/net/wkhan/naturesaura_plus/data/config/AuraGenConfig.java
@@ -7,72 +7,119 @@ import net.minecraftforge.fml.event.config.ModConfigEvent;
 import net.wkhan.naturesaura_plus.NaturesAuraPlus;
 
 @Mod.EventBusSubscriber(modid = NaturesAuraPlus.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class AuraGenConfig
-{
+public class AuraGenConfig {
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec.IntValue FLOWER_GEN_RANGE = BUILDER
-            .comment("Horizontal range in blocks of the flower generator. (Default: 3) \n(Note, does not affect vertical range)")
-            .defineInRange("flowerGenRange", 3, 1, 10);
+    // Flower Generator
+    public static final ForgeConfigSpec.IntValue FLOWER_GEN_RANGE;
+    public static final ForgeConfigSpec.IntValue FLOWER_GEN_MEMORY_SIZE;
+    public static final ForgeConfigSpec.IntValue FLOWER_GEN_VITALITY_FLOOR;
+    public static final ForgeConfigSpec.DoubleValue FLOWER_GEN_POW_FACTOR;
+
+    // Moss Generator
+    public static final ForgeConfigSpec.IntValue MOSS_GEN_RANGE;
+    public static final ForgeConfigSpec.IntValue MOSS_GEN_MEMORY_SIZE;
+
+    // Animal Generator
+    public static final ForgeConfigSpec.IntValue ANIMAL_GEN_RANGE;
+
+    // Slime Generator
+    public static final ForgeConfigSpec.IntValue SLIME_GEN_RANGE;
+
+    // Chorus Generator
+    public static final ForgeConfigSpec.IntValue CHORUS_GEN_RANGE;
+
+    // Oak Generator
+    public static final ForgeConfigSpec.IntValue OAK_GEN_RANGE;
+
+    // Potion Generator
+    public static final ForgeConfigSpec.IntValue POTION_GEN_RANGE;
+    public static final ForgeConfigSpec.IntValue POTION_CAP_PER_TICK;
+    public static final ForgeConfigSpec.BooleanValue CHECK_MULTI_FOR_POTION_GEN;
+
+    // Firework Generator
+    public static final ForgeConfigSpec.IntValue FIREWORK_GEN_RANGE;
+    static {
+        BUILDER.comment("Settings for the Flower Aura Generator").push("FlowerGenerator");
+        FLOWER_GEN_RANGE = BUILDER
+                .comment("Horizontal range in blocks of the flower generator. (Default: 3) \n(Note, does not affect vertical range)")
+                .defineInRange("flowerGenRange", 3, 1, 10);
+        FLOWER_GEN_MEMORY_SIZE = BUILDER
+                .comment("Number of flowers the flower aura generator block remembers when calculating aura generated amount. (Default: 3)")
+                .defineInRange("flowerGenMemorySize", 3, 1, 1000);
+        FLOWER_GEN_VITALITY_FLOOR = BUILDER
+                .comment("Defines the lowest value of vitality upto which the flower generator generates aura. (Default: 100)")
+                .defineInRange("flowerGenVitalityFloor", 100, 0, 100);
+        FLOWER_GEN_POW_FACTOR = BUILDER
+                .comment("The value of the power factor in the aura generation formula for flower generator. Check wiki for more info. (Default: 0.5)")
+                .defineInRange("flowerGenPowFactor", 0.5, -5.0, 10.0);
+        BUILDER.pop();
 
 
-    public static final ForgeConfigSpec.IntValue FLOWER_GEN_MEMORY_SIZE = BUILDER
-            .comment("Number of flowers the flower aura generator block remembers when calculating aura generated amount. (Default: 3)")
-            .defineInRange("flowerGenMemorySize", 3, 1, 1000);
+        BUILDER.comment("Settings for the Moss Aura Generator").push("MossGenerator");
+        MOSS_GEN_RANGE = BUILDER
+                .comment("Horizontal range in blocks of the moss generator. (Default: 2) \n(Note, does not affect vertical range)")
+                .defineInRange("mossGenRange", 2, 1, 10);
+        MOSS_GEN_MEMORY_SIZE = BUILDER
+                .comment("Number of mosses the moss aura generator block remembers when determining if moss block is recent (Hence skipping if recent). (Default: 3)")
+                .defineInRange("mossGenMemorySize", 3, 1, 1000);
+        BUILDER.pop();
 
-    public static final ForgeConfigSpec.IntValue FLOWER_GEN_VITALITY_FLOOR = BUILDER //clarify
-            .comment("Defines the lowest value of vitality upto which the flower generator generates aura. (Default: 100)")
-            .defineInRange("flowerGenVitalityFloor", 100, 0, 100);
 
-    public static final ForgeConfigSpec.DoubleValue FLOWER_GEN_POW_FACTOR = BUILDER
-            .comment("The value of the power factor in the aura generation formula for flower generator. Check wiki for more info. (Default: 0.5)")
-            .defineInRange("flowerGenPowFactor", 0.5, -5, 10);
+        BUILDER.comment("Settings for the Animal Aura Generator").push("AnimalGenerator");
+        ANIMAL_GEN_RANGE = BUILDER
+                .comment("Cubical size radius (half-length) for range of the animal generator in blocks. (Default: 5)")
+                .defineInRange("animalGenRange", 5, 1, 10);
+        BUILDER.pop();
 
-    public static final ForgeConfigSpec.IntValue MOSS_GEN_RANGE = BUILDER
-            .comment("Horizontal range in blocks of the moss generator. (Default: 2) \n(Note, does not affect vertical range)")
-            .defineInRange("mossGenRange", 2, 1, 10);
 
-    public static final ForgeConfigSpec.IntValue MOSS_GEN_MEMORY_SIZE = BUILDER
-            .comment("Number of mosses the moss aura generator block remembers when determining if moss block is recent (Hence skipping if recent). (Default: 3)")
-            .defineInRange("mossGenMemorySize", 3, 1, 1000);
+        BUILDER.comment("Settings for the Slime Aura Generator").push("SlimeGenerator");
+        SLIME_GEN_RANGE = BUILDER
+                .comment("Cubical size radius (half-length) for range of the slime generator in blocks. (Default: 8)")
+                .defineInRange("slimeGenRange", 8, 1, 32);
+        BUILDER.pop();
 
-    public static final ForgeConfigSpec.IntValue ANIMAL_GEN_RANGE = BUILDER
-            .comment("Cubical size radius (half-length) for range of the animal generator in blocks. (Default: 5)")
-            .defineInRange("animalGenRange", 5, 1, 10);
 
-    public static final ForgeConfigSpec.IntValue SLIME_GEN_RANGE = BUILDER
-            .comment("Cubical size radius (half-length) for range of the slime generator in blocks. (Default: 8)")
-            .defineInRange("slimeGenRange", 8, 1, 32);
+        BUILDER.comment("Settings for the Chorus Aura Generator").push("ChorusGenerator");
+        CHORUS_GEN_RANGE = BUILDER
+                .comment("Cubical size radius (half-length) for range of the chorus generator to check for valid plants in blocks. (Default: 2)")
+                .defineInRange("chorusGenRange", 2, 1, 10);
+        BUILDER.pop();
 
-    public static final ForgeConfigSpec.IntValue CHORUS_GEN_RANGE = BUILDER
-            .comment("Cubical size radius (half-length) for range of the chorus generator to check for valid plants in blocks. (Default: 2)")
-            .defineInRange("chorusGenRange", 2, 1, 10);
 
-    public static final ForgeConfigSpec.IntValue OAK_GEN_RANGE = BUILDER
-            .comment("Cubical size radius (half-length) for range of the oak generator to check for tree/feature placements. (Default: 10)")
-            .defineInRange("oakGenRange", 10, 1, 32);
+        BUILDER.comment("Settings for the Oak Aura Generator").push("OakGenerator");
+        OAK_GEN_RANGE = BUILDER
+                .comment("Cubical size radius (half-length) for range of the oak generator to check for tree/feature placements. (Default: 10)")
+                .defineInRange("oakGenRange", 10, 1, 32);
+        BUILDER.pop();
 
-    public static final ForgeConfigSpec.IntValue POTION_GEN_RANGE = BUILDER
-            .comment("Cubical size radius (half-length) for range of the potion generator to check for lingering potions. (Default: 2)")
-            .defineInRange("potionGenRange", 2, 1, 10);
 
-    public static final ForgeConfigSpec.IntValue POTION_CAP_PER_TICK = BUILDER
-            .comment("Number of lingering potions the potion generator will accept for aura generation per tick. \nSet to -1 to remove the cap. (Default: 2)")
-            .defineInRange("potionCapForGenPerTick", -1, -1, 100);
+        BUILDER.comment("Settings for the Potion Aura Generator").push("PotionGenerator");
+        POTION_GEN_RANGE = BUILDER
+                .comment("Cubical size radius (half-length) for range of the potion generator to check for lingering potions. (Default: 2)")
+                .defineInRange("potionGenRange", 2, 1, 10);
+        POTION_CAP_PER_TICK = BUILDER
+                .comment("Number of lingering potions the potion generator will accept for aura generation per tick. \nSet to -1 to remove the cap. (Default: 2)")
+                .defineInRange("potionCapForGenPerTick", -1, -1, 100);
+        CHECK_MULTI_FOR_POTION_GEN = BUILDER
+                .comment("Whether the potion generator needs its multiblock to function or not. (Default: true)")
+                .define("checkMultiForPotionGen", true);
+        BUILDER.pop();
 
-    public static final ForgeConfigSpec.BooleanValue CHECK_MULTI_FOR_POTION_GEN = BUILDER
-            .comment("Whether the potion generator needs its multiblock to function or not. (Default: true)")
-            .define("checkMultiForPotionGen", true);
 
-    public static final ForgeConfigSpec.IntValue FIREWORK_GEN_RANGE = BUILDER
-            .comment("Cubical size radius (half-length) for range of the firework generator to check for dropped fireworks. (Default: 10)")
-            .defineInRange("fireworkGenRange", 4, 1, 32);
+        BUILDER.comment("Settings for the Firework Aura Generator").push("FireworkGenerator");
+        FIREWORK_GEN_RANGE = BUILDER
+                .comment("Cubical size radius (half-length) for range of the firework generator to check for dropped fireworks. (Default: 10)")
+                .defineInRange("fireworkGenRange", 4, 1, 32);
+        BUILDER.pop();
 
-    public static final ForgeConfigSpec SPEC = BUILDER.build();
+        SPEC = BUILDER.build();
+    }
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
-        if(event.getConfig().getSpec() != SPEC) return;
+        if (event.getConfig().getSpec() != SPEC) return;
 
     }
 }
--- src/main/java/net/wkhan/naturesaura_plus/data/config/GameplayConfig.java
+++ src/main/java/net/wkhan/naturesaura_plus/data/config/GameplayConfig.java
@@ -8,95 +8,118 @@ import net.wkhan.naturesaura_plus.NaturesAuraPlus;
 
 @Mod.EventBusSubscriber(modid = NaturesAuraPlus.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class GameplayConfig {
-
     private static final ForgeConfigSpec.Builder BUILDER = new ForgeConfigSpec.Builder();
+    public static final ForgeConfigSpec SPEC;
 
-    public static final ForgeConfigSpec.IntValue BREAK_PREVENTION_APPLY_COST = BUILDER
-            .comment("Levels cost required to apply Token of Fortitude to an item. (Default: 30)")
-            .defineInRange("breakPreventionApplyCost", 30, 1, 1000);
+    // Tools
+    public static final ForgeConfigSpec.IntValue BREAK_PREVENTION_APPLY_COST;
+    public static final ForgeConfigSpec.IntValue LOOT_FINDER_AURA_COST;
+    public static final ForgeConfigSpec.IntValue LOOT_FINDER_RANGE;
+    public static final ForgeConfigSpec.IntValue LOOT_FINDER_USE_COOLDOWN;
+    public static final ForgeConfigSpec.IntValue LOOT_FINDER_LIGHT_LIFE;
+    public static final ForgeConfigSpec.IntValue PET_RECALL_RANGE;
+    public static final ForgeConfigSpec.IntValue SASH_AURA_CAPACITY;
+    public static final ForgeConfigSpec.IntValue SASH_MANA_CAPACITY;
 
-    public static final ForgeConfigSpec.IntValue LOOT_FINDER_AURA_COST = BUILDER
-            .comment("The aura cost of using the loot finder item. (Default: 100,000)")
-            .defineInRange("lootFinderAuraCost", 100000, 0, 1200000);
+    // Furnace Booster
+    public static final ForgeConfigSpec.IntValue FURNACE_BOOSTER_AURA_COST;
+    public static final ForgeConfigSpec.BooleanValue CHECK_TAG_FOR_FURNACE_BOOST;
+    public static final ForgeConfigSpec.IntValue FURNACE_BOOSTER_CHANCE;
 
-    public static final ForgeConfigSpec.IntValue LOOT_FINDER_RANGE = BUILDER
-            .comment("Maximum range (in blocks) upto which loot finder item can detect treasure. (Default: 64)")
-            .defineInRange("lootFinderRange", 64, 0, 1024);
+    // Bountiful Core
+    public static final ForgeConfigSpec.BooleanValue CUBICAL_ORE_SPAWN;
+    public static final ForgeConfigSpec.IntValue MIN_AURA_FOR_ORE_SPAWN;
+    public static final ForgeConfigSpec.IntValue MAX_ITERATION_FOR_ORE_SPAWN;
+    public static final ForgeConfigSpec.DoubleValue ORE_SPAWN_ITER_SCALE;
+    public static final ForgeConfigSpec.DoubleValue ORE_SPAWN_RANGE_SCALE;
+    public static final ForgeConfigSpec.IntValue MIN_ORE_SPAWN_RANGE;
+    public static final ForgeConfigSpec.IntValue MAX_ORE_SPAWN_RANGE;
+    public static final ForgeConfigSpec.IntValue MAX_ORE_PLACE_ATTEMPTS;
 
-    public static final ForgeConfigSpec.IntValue LOOT_FINDER_USE_COOLDOWN = BUILDER
-            .comment("Cooldown set on loot finder item upon use, in ticks. (Default: 1,200)")
-            .defineInRange("lootFinderUseCooldownInTicks", 1200, 0, 72000);
-
-    public static final ForgeConfigSpec.IntValue LOOT_FINDER_LIGHT_LIFE = BUILDER
-            .comment("How long the loot finder particles stay, in ticks. (Default: 1,200)")
-            .defineInRange("lootFinderLightLifeInTicks", 1200, 0, 72000);
-
-    public static final ForgeConfigSpec.IntValue PET_RECALL_RANGE = BUILDER
-            .comment("Maximum range upto which pets are selected to be teleported alongside owner upon using aura coffee. (Default: 4)")
-            .defineInRange("petRecallRange", 4, 1, 64);
-
-    public static final ForgeConfigSpec.IntValue SASH_AURA_CAPACITY = BUILDER
-            .comment("Aura capacity for the Naturalist Sash. (Default: 1,200,000)")
-            .defineInRange("sashAuraCapacity", 1200000, 1, Integer.MAX_VALUE);
-
-    public static final ForgeConfigSpec.IntValue SASH_MANA_CAPACITY = BUILDER
-            .comment("Mana capacity for the Naturalist Sash. (Default: 2,000,000)")
-            .defineInRange("sashManaCapacity", 2000000, 1, Integer.MAX_VALUE);
-
-    public static final ForgeConfigSpec.IntValue FURNACE_BOOSTER_AURA_COST = BUILDER
-            .comment("The aura cost per recipe for the furnace booster block. (Default: 6,500)")
-            .defineInRange("furnaceBoosterAuraCost", 6500, 0, 1000000);
-
-    public static final ForgeConfigSpec.BooleanValue CHECK_TAG_FOR_FURNACE_BOOST = BUILDER
-            .comment("Whether the furnace booster should check the input item in furnace against the tag #naturesaura_plus:valid_smeltable_to_boost,\n to decide whether to double the smelted output or not (Default: false)")
-            .define("allowAllForFurnaceBoost", false);
-
-    public static final ForgeConfigSpec.IntValue FURNACE_BOOSTER_CHANCE = BUILDER
-            .comment("The aura cost per recipe for the furnace booster block. (Default: 45)")
-            .defineInRange("furnaceBoosterChance", 45, 0, 100);
-
-    public static final ForgeConfigSpec.BooleanValue CUBICAL_ORE_SPAWN = BUILDER
-            .comment("Whether the ore spawning from Powder of the Bountiful Core applies to a cubical range (else spherical range). (Default: true)")
-            .define("isOreEffectCubical", true);
-
-    public static final ForgeConfigSpec.IntValue MIN_AURA_FOR_ORE_SPAWN = BUILDER
-            .comment("The minimum environmental aura required for Powder of the Bountiful Core to function. (Default: 2,000,000)")
-            .defineInRange("minAuraForOreSpawn", 2000000, 0, Integer.MAX_VALUE);
-
-    public static final ForgeConfigSpec.IntValue MAX_ITERATION_FOR_ORE_SPAWN = BUILDER
-            .comment("Maximum number of ores the Powder of the Bountiful Core attempts to place per operation. (Default: 20)")
-            .defineInRange("maxIterationForOreSpawn", 20, 1, Integer.MAX_VALUE);
-
-    public static final ForgeConfigSpec.DoubleValue ORE_SPAWN_ITER_SCALE = BUILDER
-            .comment("Value of oreSpawnIterationCalcScale in the formula: AuraNearby / (AuraSpotsNearby * oreSpawnIterationCalcScale),\n to determine the number of ores the Powder of the Bountiful Core attempts to place per operation. (Default: 300,000)")
-            .defineInRange("oreSpawnIterationCalcScale", 300000D, 0D, Integer.MAX_VALUE);
-
-    public static final ForgeConfigSpec.DoubleValue ORE_SPAWN_RANGE_SCALE = BUILDER
-            .comment("Value of oreSpawnRangeScale in the formula: AuraNearby / oreSpawnRangeScale, to determine the range of the Powder of the Bountiful Core. (Default: 150,000)")
-            .defineInRange("oreSpawnRangeScale", 150000D, 1D, Integer.MAX_VALUE);
-
-    public static final ForgeConfigSpec.IntValue MIN_ORE_SPAWN_RANGE = BUILDER
-            .comment("Minimum radius for range of the Powder of the Bountiful Core. (Default: 5)")
-            .defineInRange("minOreSpawnRange", 5, 1, Integer.MAX_VALUE);
-
-    public static final ForgeConfigSpec.IntValue MAX_ORE_SPAWN_RANGE = BUILDER
-            .comment("Maximum radius for range of the Powder of the Bountiful Core. (Default: 20)")
-            .defineInRange("maxOreSpawnRange", 20, 1, Integer.MAX_VALUE);
-
-    public static final ForgeConfigSpec.IntValue MAX_ORE_PLACE_ATTEMPTS = BUILDER
-            .comment("Maximum attempts for the Powder of the Bountiful Core to place an ore, before failing and passing. (Default: 10)")
-            .defineInRange("maxOrePlaceAttempts", 10, 1, 100);
-
-    public static final ForgeConfigSpec.BooleanValue ENABLE_WHITELIST_AURA_BONEMEAL = BUILDER
-            .comment("Whether the tag #naturesaura_plus:aura_bonemeal_whitelist is checked before allowing environmental bone-mealing due aura excess.\nThe tag #naturesaura_plus:aura_bonemeal_blacklist, however, still takes precedence. (Default: false)")
-            .define("useAuraBoneMealWhiteList", false);
-
-    public static final ForgeConfigSpec.IntValue AURA_BONEMEAL_COST = BUILDER
-            .comment("The aura cost per environmental aura bonemeal effect occurrence. (Default: 3500)")
-            .defineInRange("auraBoneMealCost", 3500, 1, Integer.MAX_VALUE);
+    // Aura Bone-meal
+    public static final ForgeConfigSpec.BooleanValue ENABLE_WHITELIST_AURA_BONEMEAL;
+    public static final ForgeConfigSpec.IntValue AURA_BONEMEAL_COST;
+    static {
+        BUILDER.comment("Tools").push("Tools");
+        BREAK_PREVENTION_APPLY_COST = BUILDER
+                .comment("Levels cost required to apply Token of Fortitude to an item. (Default: 30)")
+                .defineInRange("breakPreventionApplyCost", 30, 1, 1000);
+        LOOT_FINDER_AURA_COST = BUILDER
+                .comment("The aura cost of using the loot finder item. (Default: 100,000)")
+                .defineInRange("lootFinderAuraCost", 100000, 0, 1200000);
+        LOOT_FINDER_RANGE = BUILDER
+                .comment("Maximum range (in blocks) upto which loot finder item can detect treasure. (Default: 64)")
+                .defineInRange("lootFinderRange", 64, 0, 1024);
+        LOOT_FINDER_USE_COOLDOWN = BUILDER
+                .comment("Cooldown set on loot finder item upon use, in ticks. (Default: 1,200)")
+                .defineInRange("lootFinderUseCooldownInTicks", 1200, 0, 72000);
+        LOOT_FINDER_LIGHT_LIFE = BUILDER
+                .comment("How long the loot finder particles stay, in ticks. (Default: 1,200)")
+                .defineInRange("lootFinderLightLifeInTicks", 1200, 0, 72000);
+        PET_RECALL_RANGE = BUILDER
+                .comment("Maximum range upto which pets are selected to be teleported alongside owner upon using aura coffee. (Default: 4)")
+                .defineInRange("petRecallRange", 4, 1, 64);
+        SASH_AURA_CAPACITY = BUILDER
+                .comment("Aura capacity for the Naturalist Sash. (Default: 1,200,000)")
+                .defineInRange("sashAuraCapacity", 1200000, 1, Integer.MAX_VALUE);
+        SASH_MANA_CAPACITY = BUILDER
+                .comment("Mana capacity for the Naturalist Sash. (Default: 2,000,000)")
+                .defineInRange("sashManaCapacity", 2000000, 1, Integer.MAX_VALUE);
+        BUILDER.pop();
 
 
-    public static final ForgeConfigSpec SPEC = BUILDER.build();
+        BUILDER.comment("Furnace Booster").push("Furnace Booster");
+        FURNACE_BOOSTER_AURA_COST = BUILDER
+                .comment("The aura cost per recipe for the furnace booster block. (Default: 6,500)")
+                .defineInRange("furnaceBoosterAuraCost", 6500, 0, 1000000);
+        CHECK_TAG_FOR_FURNACE_BOOST = BUILDER
+                .comment("Whether the furnace booster should check the input item in furnace against the tag #naturesaura_plus:valid_smeltable_to_boost,\n to decide whether to double the smelted output or not (Default: false)")
+                .define("allowAllForFurnaceBoost", false);
+        FURNACE_BOOSTER_CHANCE = BUILDER
+                .comment("The aura cost per recipe for the furnace booster block. (Default: 45)")
+                .defineInRange("furnaceBoosterChance", 45, 0, 100);
+        BUILDER.pop();
+
+
+        BUILDER.comment("Powder of the Bountiful Core").push("BountifulCore");
+        CUBICAL_ORE_SPAWN = BUILDER
+                .comment("Whether the ore spawning from Powder of the Bountiful Core applies to a cubical range (else spherical range). (Default: true)")
+                .define("isOreEffectCubical", true);
+        MIN_AURA_FOR_ORE_SPAWN = BUILDER
+                .comment("The minimum environmental aura required for Powder of the Bountiful Core to function. (Default: 2,000,000)")
+                .defineInRange("minAuraForOreSpawn", 2000000, 0, Integer.MAX_VALUE);
+        MAX_ITERATION_FOR_ORE_SPAWN = BUILDER
+                .comment("Maximum number of ores the Powder of the Bountiful Core attempts to place per operation. (Default: 20)")
+                .defineInRange("maxIterationForOreSpawn", 20, 1, Integer.MAX_VALUE);
+        ORE_SPAWN_ITER_SCALE = BUILDER
+                .comment("Value of oreSpawnIterationCalcScale in the formula: AuraNearby / (AuraSpotsNearby * oreSpawnIterationCalcScale),\n to determine the number of ores the Powder of the Bountiful Core attempts to place per operation. (Default: 300,000)")
+                .defineInRange("oreSpawnIterationCalcScale", 300000D, 0D, Integer.MAX_VALUE);
+        ORE_SPAWN_RANGE_SCALE = BUILDER
+                .comment("Value of oreSpawnRangeScale in the formula: AuraNearby / oreSpawnRangeScale, to determine the range of the Powder of the Bountiful Core. (Default: 150,000)")
+                .defineInRange("oreSpawnRangeScale", 150000D, 1D, Integer.MAX_VALUE);
+        MIN_ORE_SPAWN_RANGE = BUILDER
+                .comment("Minimum radius for range of the Powder of the Bountiful Core. (Default: 5)")
+                .defineInRange("minOreSpawnRange", 5, 1, Integer.MAX_VALUE);
+        MAX_ORE_SPAWN_RANGE = BUILDER
+                .comment("Maximum radius for range of the Powder of the Bountiful Core. (Default: 20)")
+                .defineInRange("maxOreSpawnRange", 20, 1, Integer.MAX_VALUE);
+        MAX_ORE_PLACE_ATTEMPTS = BUILDER
+                .comment("Maximum attempts for the Powder of the Bountiful Core to place an ore, before failing and passing. (Default: 10)")
+                .defineInRange("maxOrePlaceAttempts", 10, 1, 100);
+        BUILDER.pop();
+
+
+        BUILDER.comment("Environmental Aura Bone-Meal").push("Aura Bone-Meal");
+        ENABLE_WHITELIST_AURA_BONEMEAL = BUILDER
+                .comment("Whether the tag #naturesaura_plus:aura_bonemeal_whitelist is checked before allowing environmental bone-mealing due aura excess.\nThe tag #naturesaura_plus:aura_bonemeal_blacklist, however, still takes precedence. (Default: false)")
+                .define("useAuraBoneMealWhiteList", false);
+        AURA_BONEMEAL_COST = BUILDER
+                .comment("The aura cost per environmental aura bonemeal effect occurrence. (Default: 3500)")
+                .defineInRange("auraBoneMealCost", 3500, 1, Integer.MAX_VALUE);
+        BUILDER.pop();
+
+        SPEC = BUILDER.build();
+    }
 
     @SubscribeEvent
     static void onLoad(final ModConfigEvent event) {
