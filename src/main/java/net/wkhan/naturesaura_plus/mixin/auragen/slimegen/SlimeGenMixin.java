9fcb2b1abdf781a83bc80a7656f5d88b6e06f4dd
Waqar Ahmad Khan <wak4746@gmail.com>
2026-09-15 21:24:10 +0530

    removed unnecessary slime gen error print

--- src/main/java/net/wkhan/naturesaura_plus/mixin/auragen/slimegen/SlimeGenMixin.java
+++ src/main/java/net/wkhan/naturesaura_plus/mixin/auragen/slimegen/SlimeGenMixin.java
@@ -10,7 +10,7 @@ import net.minecraft.world.entity.monster.Slime;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import net.wkhan.naturesaura_plus.data.auragen.AuraGenRules;
-import net.wkhan.naturesaura_plus.data.duckfaces.SlimeGeneration;
+import net.wkhan.naturesaura_plus.data.duckfaces.ISlimeGeneration;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,7 +20,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import static net.wkhan.naturesaura_plus.data.auragen.AuraGenRules.SLIME_GENERATIONS;
 
 @Mixin(BlockEntitySlimeSplitGenerator.class)
-public abstract class SlimeGenMixin extends BlockEntityImpl implements SlimeGeneration {
+public abstract class SlimeGenMixin extends BlockEntityImpl implements ISlimeGeneration {
     public SlimeGenMixin(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
@@ -37,8 +37,10 @@ public abstract class SlimeGenMixin extends BlockEntityImpl implements SlimeGene
     )
     private void naturesaura_plus$slimeTileAuraGeneratorTick(CallbackInfo ci) {
         ci.cancel();
-        if (this.level.isClientSide() || this.level.getGameTime() % 10L != 0L) return;
-        if (this.generationTimer <= 0) return;
+        if (this.level == null || this.level.isClientSide() || this.level.getGameTime() % 10L != 0L)
+            return;
+        if (this.generationTimer <= 0)
+            return;
         int amount = this.amountToRelease;
         if (this.canGenerateRightNow(amount)) {
             this.generateAura(amount);
@@ -51,17 +53,15 @@ public abstract class SlimeGenMixin extends BlockEntityImpl implements SlimeGene
     public void naturesaura_plus$slimeTileAuraGeneratorStart(Entity entity) {
         AuraGenRules.SlimeValues slimeValues = SLIME_GENERATIONS.get(entity.getType());
         float size;
-        size = slimeValues.sizeModifier();
-        if (slimeValues.doSlimeSizeScaling()) {
-            if (entity instanceof Slime slime) size = slime.getSize();
-            else System.err.println("Entity is not a slime! Type: " + entity.getType() + "\nUsing size_modifier field as size instead.");
-        }
+        size = slimeValues.doSlimeSizeScaling() && entity instanceof Slime slime ? slime.getSize() : slimeValues.sizeModifier();
         int genTime;
         if (slimeValues.isFlatGenerationTimer()) genTime = slimeValues.flatGenerationTimer();
         else genTime = Math.round(size * slimeValues.generationTimerModifier());
         this.generationTimer = genTime;
         this.amountToRelease = Math.round(size * slimeValues.auraAmount() / this.generationTimer);
         this.color = slimeValues.slimeColor();
+        if (this.level == null)
+            return;
         PacketHandler.sendToAllAround(this.level, this.worldPosition, 32,
                 new PacketParticles(
                         (float)entity.getX(), (float)entity.getY(), (float)entity.getZ(), PacketParticles.Type.SLIME_SPLIT_GEN_START,
