d6a23ef3197c54725b8f925ee294caf78e586606
Waqar Ahmad Khan <wak4746@gmail.com>
2026-09-16 20:42:18 +0530

    moved ancient log variants data gen to own class

---
+++ src/main/java/net/wkhan/naturesaura_plus/datagen/ModBlockStateProviderNA.java
@@ -0,0 +1,23 @@
+package net.wkhan.naturesaura_plus.datagen;
+
+import net.minecraft.data.PackOutput;
+import net.minecraft.world.level.block.RotatedPillarBlock;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.wkhan.naturesaura_plus.common.block.ModBlocks;
+
+public class ModBlockStateProviderNA extends BlockStateProvider {
+    public ModBlockStateProviderNA(PackOutput output, String MODID, ExistingFileHelper exFileHelper) {
+        super(output, MODID, exFileHelper);
+    }
+
+    @Override
+    protected void registerStatesAndModels() {
+        logBlock((RotatedPillarBlock) ModBlocks.STRIPPED_ANCIENT_LOG.get());
+        axisBlock((RotatedPillarBlock) ModBlocks.STRIPPED_ANCIENT_BARK.get(),
+                modLoc("block/stripped_ancient_log"), modLoc("block/stripped_ancient_log"));
+
+        simpleBlockItem(ModBlocks.STRIPPED_ANCIENT_LOG.get(), models().getExistingFile(modLoc("block/stripped_ancient_log")));
+        simpleBlockItem(ModBlocks.STRIPPED_ANCIENT_BARK.get(), models().getExistingFile(modLoc("block/stripped_ancient_bark")));
+    }
+}
